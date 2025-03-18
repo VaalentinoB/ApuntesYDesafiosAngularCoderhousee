985ae3eb685121aab7413846c07d4cdeb3949572
Valentino Burioni <118487875+VaalentinoB@users.noreply.github.com>
2025-03-18 12:09:14 -0300

    Update: new portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { MainContentComponent } from './components/main-content/main-content.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { ContentComponent } from './components/content/content.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     SidebarComponent,
-    MainContentComponent,
-    FooterComponent
+    ToolbarComponent,
+    ContentComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/components/toolbar/toolbar.component.spec.ts
+++ src/app/components/toolbar/toolbar.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MainContentComponent } from './main-content.component';
+import { ToolbarComponent } from './toolbar.component';
 
-describe('MainContentComponent', () => {
-  let component: MainContentComponent;
-  let fixture: ComponentFixture<MainContentComponent>;
+describe('ToolbarComponent', () => {
+  let component: ToolbarComponent;
+  let fixture: ComponentFixture<ToolbarComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [MainContentComponent]
+      declarations: [ToolbarComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(MainContentComponent);
+    fixture = TestBed.createComponent(ToolbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
