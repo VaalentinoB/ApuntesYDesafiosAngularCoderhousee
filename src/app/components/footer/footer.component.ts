fa9d453f237b106d4d8c4aa674c3f90d0e3d91dc
Valentino Burioni <118487875+VaalentinoB@users.noreply.github.com>
2025-03-18 11:45:38 -0300

    update: footer

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: false,
+  template: `
+    <p>
+      Soy el footer
+    </p>
+  `,
+  styles: ``
+})
+export class FooterComponent {
+
+}
